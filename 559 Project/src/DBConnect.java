7c24275b95738633b14369be30ba6e0fb37eba57
nicswedak <nic-175@hotmail.com>
2016-03-03 12:45:30 -0700

    Initial commit of the Database connect, database queries, and runner for database logic.

---
+++ 559 Project/src/DBConnect.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnect {
+
+	public Connection SQLConnection() {
+		Connection con = null;
+		try {
+			String host = "jdbc:mysql://localhost:3306/ratemyprof";
+			String username = "root";
+			String password = "nic12345";
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection(host, username, password);
+			System.out.println("Successfuly connected to: " + host);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch ( SQLException err ) {
+			System.out.println( err.getMessage( ) );
+		}
+		return con;
+	}
+}
+
